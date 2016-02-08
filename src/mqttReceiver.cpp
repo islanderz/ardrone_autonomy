4b4622475c424ee0e8bfbb7d1cf51e2d20759def
Harshit Sureka <hs29590@gmail.com>
2016-02-08 18:27:04 +0530

    Changing fields of navdata and populating only those required by tum_ardrone package

--- src/mqttReceiver.cpp
+++ src/mqttReceiver.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "MQTTAsync.h"
+#include <ardrone_autonomy/Navdata.h>
 #include "zlib.h"
 extern "C" {
 #include "binn.h"
@@ -73,6 +74,10 @@ class callback : public virtual mqtt::callback,
 	mqtt::async_client& cli_;
 	action_listener& listener_;
 
+  //ROS Topic Publisher
+  ros::NodeHandle nh_;
+  ros::Publisher navdataPub_;
+
 	void reconnect() {
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		mqtt::connect_options connOpts;
@@ -122,11 +127,9 @@ class callback : public virtual mqtt::callback,
 
       obj = binn_open((void*)(msg->get_payload()).data());
 
-      uint32_t timestamp = binn_object_uint32(obj,(char*)"timestamp");
-      uint16_t tag = binn_object_uint16(obj, (char*)"tag");
-      uint16_t size = binn_object_uint16(obj, (char*)"size");
-      uint32_t ctrl_state = binn_object_uint32(obj, (char*)"ctrl_state");
       uint32_t vbat_flying_percentage = binn_object_uint32(obj, (char*)"vbat_flying_percentage");
+      uint32_t ctrl_state = binn_object_uint32(obj, (char*)"ctrl_state");
+      int32_t pressure = binn_object_uint32(obj, (char*)"pressure");
       float theta = binn_object_float(obj, (char*)"theta");
       float phi = binn_object_float(obj, (char*)"phi");
       float psi = binn_object_float(obj, (char*)"psi");
@@ -134,8 +137,11 @@ class callback : public virtual mqtt::callback,
       float vx = binn_object_float(obj, (char*)"vx");
       float vy = binn_object_float(obj, (char*)"vy");
       float vz = binn_object_float(obj, (char*)"vz");
-      uint32_t num_frames = binn_object_uint32(obj, (char*)"num_frames");
-      uint32_t detection_camera_type = binn_object_uint32(obj, (char*)"detection_camera_type");
+      uint32_t motor1 = binn_object_uint32(obj, (char*)"motor1");
+      uint32_t motor2 = binn_object_uint32(obj, (char*)"motor2");
+      uint32_t motor3 = binn_object_uint32(obj, (char*)"motor3");
+      uint32_t motor4 = binn_object_uint32(obj, (char*)"motor4");
+      uint32_t tm = binn_object_uint32(obj, (char*)"tm");
 
       printf("Received navdata msg with angles: %f %f %f\n", theta, phi, psi);
       printf("Received navdata msg with velocities: %f %f %f\n", vx, vy, vz);
@@ -143,6 +149,30 @@ class callback : public virtual mqtt::callback,
       printf("\n");
 
       //NEED TO PUBLISH ON ROS TOPICS HERE
+
+      //conversions from ardrone_driver.cpp
+      ardrone_autonomy::Navdata navMsg;
+
+      navMsg.header.stamp = ros::Time::now();
+      navMsg.batteryPercent = vbat_flying_percentage;
+      navMsg.state = ctrl_state >> 16;
+
+      // positive means counterclockwise rotation around axis
+      navMsg.rotX = phi / 1000.0;  // tilt left/right
+      navMsg.rotY = theta / 1000.0;  // tilt forward/backward
+      navMsg.rotZ = psi / 1000.0;  // orientation
+
+      navMsg.altd = altitude;  // cm
+      navMsg.vx = vx;  // mm/sec
+      navMsg.vy = -vy;  // mm/sec
+      navMsg.vz = -vz;  // mm/sec
+      navMsg.motor1 = motor1;
+      navMsg.motor2 = motor2;
+      navMsg.motor3 = motor3;
+      navMsg.motor4 = motor4;
+      navMsg.tm = (tm & 0x001FFFFF) + (tm >> 21) * 1000000;
+
+      navdataPub_.publish(navMsg);
 
       binn_free(obj);
       return;
@@ -153,198 +183,20 @@ class callback : public virtual mqtt::callback,
 	virtual void delivery_complete(mqtt::idelivery_token_ptr token) {}
 
 public:
-	callback(mqtt::async_client& cli, action_listener& listener) 
-				: cli_(cli), listener_(listener) {}
+	callback(mqtt::async_client& cli, action_listener& listener, ros::NodeHandle nh) 
+				: cli_(cli), listener_(listener), nh_(nh) {}
+
+  void setNavdataPublisher()
+  {
+    navdataPub_ = nh_.advertise<ardrone_autonomy::Navdata>("ardrone/navdata", 200);
+  }
 };
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-/*
-class MQTTROSBridge
-{
-  public:
-  MQTTROSBridge();
-  ~MQTTROSBridge();
-
-  MQTTROSBridge(std::string address)
-  {
-    MQTTAsync_create(&client, address.c_str(), CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    connected = 0;
-    finished = 0;
-  }
-    
-
-
-  int disc_finished;
-  int subscribed;
-  int connected;
-  int finished;
-
-  MQTTAsync client;
-
-  void setCallbacks()
-  {
-    MQTTAsync_setCallbacks(client, NULL, connlost, msgarrvd, NULL);
-  }
-
-  void connlost(void *context, char *cause)
-  {
-    MQTTAsync client = (MQTTAsync)context;
-    MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
-    int rc;
-
-    ROS_INFO("\nConnection lost\n");
-    ROS_INFO("     cause: %s\n", cause);
-
-    ROS_INFO("Reconnecting\n");
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
-    if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS)
-    {
-      ROS_INFO("Failed to start connect, return code %d\n", rc);
-      finished = 1;
-    }
-  }
-
-
-  int msgarrvd(void *context, char *topicName, int topicLen, MQTTAsync_message *message)
-  {
-    //we have received a compressed image stream and need to decompress it
-    if(strcmp(topicName,"image/compressedImageStream") == 0)
-    {
-      int MAX_MSG_SIZE = 50000;//this value should be updated and stored as a global variable. Or hardcoded if we know the max size.
-      uint8_t* imgData;
-      unsigned long uncompSize;
-      imgData = (uint8_t*)malloc(MAX_MSG_SIZE);
-      uncompress(imgData, 
-          &uncompSize, 
-          (uint8_t*)message->payload, 
-          message->payloadlen);
-
-      printf("Received image with %d bytes, uncompressed to %ld bytes\n",message->payloadlen, uncompSize);
-      // printf("Message is: %s\n",(char*)imgData);
-      MQTTAsync_freeMessage(&message);
-      MQTTAsync_free(topicName);
-      return 1;
-    }
-    if(strcmp(topicName,"uas/ardrone1/navdata") == 0)
-    {
-      binn* obj;
-
-      obj = binn_open(message->payload);
-
-      uint32_t timestamp = binn_object_uint32(obj,(char*)"timestamp");
-      uint16_t tag = binn_object_uint16(obj, (char*)"tag");
-      uint16_t size = binn_object_uint16(obj, (char*)"size");
-      uint32_t ctrl_state = binn_object_uint32(obj, (char*)"ctrl_state");
-      uint32_t vbat_flying_percentage = binn_object_uint32(obj, (char*)"vbat_flying_percentage");
-      float theta = binn_object_float(obj, (char*)"theta");
-      float phi = binn_object_float(obj, (char*)"phi");
-      float psi = binn_object_float(obj, (char*)"psi");
-      uint32_t altitude = binn_object_uint32(obj, (char*)"altitude");
-      float vx = binn_object_float(obj, (char*)"vx");
-      float vy = binn_object_float(obj, (char*)"vy");
-      float vz = binn_object_float(obj, (char*)"vz");
-      uint32_t num_frames = binn_object_uint32(obj, (char*)"num_frames");
-      uint32_t detection_camera_type = binn_object_uint32(obj, (char*)"detection_camera_type");
-
-      printf("Received navdata msg with angles: %f %f %f\n", theta, phi, psi);
-      printf("Received navdata msg with velocities: %f %f %f\n", vx, vy, vz);
-      printf("Received navdata msg with battery/ctrl_state/altd: %d/%d/%d\n", vbat_flying_percentage, ctrl_state, altitude);
-      printf("\n");
-
-      //NEED TO PUBLISH ON ROS TOPICS HERE
-
-      binn_free(obj);
-      return 1;
-    }
-
-
-    int i;
-    char* payloadptr;
-
-    ROS_INFO("Received message of length %d on topic %s\n",message->payloadlen,topicName);
-
-    ROS_INFO("Message arrived\n");
-    ROS_INFO("     topic: %s\n", topicName);
-    ROS_INFO("   message: ");
-
-    payloadptr = (char*)message->payload;
-    for(i=0; i<message->payloadlen; i++)
-    {
-      putchar(*payloadptr++);
-    }
-    putchar('\n');
-    MQTTAsync_freeMessage(&message);
-    MQTTAsync_free(topicName);
-    return 1;
-  }
-
-
-  void onDisconnect(void* context, MQTTAsync_successData* response)
-  {
-    ROS_INFO("Successful disconnection\n");
-    disc_finished = 1;
-  }
-
-
-  void onSubscribe(void* context, MQTTAsync_successData* response)
-  {
-    ROS_INFO("Subscribe succeeded\n");
-    subscribed = 1;
-  }
-
-  void onSubscribeFailure(void* context, MQTTAsync_failureData* response)
-  {
-    ROS_INFO("Subscribe failed, rc %d\n", response ? response->code : 0);
-    finished = 1;
-  }
-
-
-  void onConnectFailure(void* context, MQTTAsync_failureData* response)
-  {
-    ROS_INFO("Connect failed, rc %d\n", response ? response->code : 0);
-    finished = 1;
-  }
-
-
-  void onConnect(void* context, MQTTAsync_successData* response)
-  {
-    ROS_INFO("Successful connection\n");
-    connected = 1;
-  }
-
-};
-
-
-//volatile MQTTAsync_token deliveredtoken;
-
-*/
-/*******************************/
-
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "mqttReceiver");
-  ros::NodeHandle n;
+  ros::NodeHandle nodeHandle;
 
-  /*********
-  std::string aa;
-  float amin = 0.0;
-  std::vector<std::string> myvec;
-  ros::param::get("/mqttReceiver/myparam",aa);
-  ros::param::get("/mqttReceiver/altitude_min",amin);
-  ros::param::get("/mqttReceiver/listTopics",myvec);
-  std::cout << myvec.size() << std::endl;
-  std::cout << "string: " << aa << std::endl;
-  std::cout << "num: " << amin << std::endl;
-  **********/
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-  
   std::string broker = "tcp://unmand.io";
   std::string brokerPort = "1884";
   ros::param::get("/mqttReceiver/mqttBroker",broker);
@@ -355,8 +207,12 @@ int main(int argc, char **argv)
   mqtt::async_client client(address.c_str(), CLIENTID);
   action_listener subListener("Subscription");
 
-  callback cb(client, subListener);
+  callback cb(client, subListener,nodeHandle);
   client.set_callback(cb);
+
+  //initialize the navdata publisher
+  cb.setNavdataPublisher();
+    
 
   mqtt::connect_options connOpts;
   connOpts.set_keep_alive_interval(20);
@@ -393,51 +249,7 @@ int main(int argc, char **argv)
   std::cout << "Disconnect: OK" << std::endl;
 
   return 0;
-  /*
-
-     MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
-  MQTTAsync_disconnectOptions disc_opts = MQTTAsync_disconnectOptions_initializer;
-  MQTTAsync_token token;
-  int rc;
   
-  
-  MQTTROSBridge mqttRosBridge(address);
-
-  ROS_INFO("Connecting to ... %s", address.c_str());
-
-  mqttRosBridge.setCallbacks();
-
-  conn_opts.keepAliveInterval = 20;
-  conn_opts.cleansession = 1;
-  conn_opts.onSuccess = &MQTTROSBridge::onConnect;
-  conn_opts.onConnectFailure = &MQTTROSBridge::onConnectFailure;
-  conn_opts.context = client;
-  if((rc = MQTTAsync_connect(mqttRosBridge.client, &conn_opts)) != MQTTASYNC_SUCCESS)
-  {
-    ROS_ERROR("Failed to start connect, return code %d\n", rc);
-  }
-  while(!mqttRosBridge.connected)
-  {
-    sleep(10);
-  }
-	*/
-/*
-  MQTTAsync_responseOptions subs_opts = MQTTAsync_responseOptions_initializer; 
-  ROS_INFO("Subscribing to %d topics...", topicsList.size());
-  subs_opts.onSuccess = &mqttRosBridge::onSubscribe;
-  subs_opts.onFailure = &mqttRosBrige::onSubscribeFailure;
-  subs_opts.context = mqttRosBridge.client;
-
-  //subscripe to all topics in the param listTopics
-  for(int i = 0; i < topicsList.size(); i++)
-  {
-    if ((rc = MQTTAsync_subscribe(mqttRosBridge.client, topicsList[i].c_str(), QOS, &subs_opts)) != MQTTASYNC_SUCCESS)
-    {
-      ROS_INFO("Failed to start subscribe, return code %d\n", rc);
-    }
-  }
-*/
-
   ROS_INFO("Exited Loop\n");
   ros::spin();
 
